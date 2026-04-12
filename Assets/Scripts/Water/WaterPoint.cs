9e6fe4cb9c2dbf5a525ef31d77d515e931b4c17d
wzr26 <127534016+wzr26@users.noreply.github.com>
2026-04-12 10:55:39 +0700

    Update WaterPoint.cs

--- Assets/Scripts/Water/WaterPoint.cs
+++ Assets/Scripts/Water/WaterPoint.cs
@@ -1,1 +1,24 @@
+using UnityEngine;
 
+public class WaterPoint
+{
+    public float height;
+    public float velocity;
+
+    public float targetHeight;
+
+    public WaterPoint(float h)
+    {
+        height = h;
+        targetHeight = h;
+        velocity = 0;
+    }
+
+    public void Update(float tension, float damping)
+    {
+        float force = tension * (targetHeight - height);
+        velocity += force;
+        velocity *= (1 - damping);
+        height += velocity;
+    }
+}
