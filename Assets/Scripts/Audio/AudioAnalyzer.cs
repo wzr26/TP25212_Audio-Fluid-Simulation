d5302db0c982e2f51802da81f915b914522fcca8
wzr26 <127534016+wzr26@users.noreply.github.com>
2026-04-12 11:36:34 +0700

    Update AudioAnalyzer.cs

--- Assets/Scripts/Audio/AudioAnalyzer.cs
+++ Assets/Scripts/Audio/AudioAnalyzer.cs
@@ -1,1 +1,23 @@
+using UnityEngine;
 
+public class AudioAnalyzer : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public float[] spectrum = new float[256];
+
+    public static float audioValue;
+
+    void Update()
+    {
+        audioSource.GetSpectrumData(spectrum, 0, FFTWindow.Blackman);
+
+        float sum = 0;
+
+        for (int i = 0; i < spectrum.Length; i++)
+        {
+            sum += spectrum[i];
+        }
+
+        audioValue = sum * 20f;
+    }
+}
