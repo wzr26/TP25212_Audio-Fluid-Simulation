ee9761e7968593585958893cbb354d11e1c75f5a
wzr26 <127534016+wzr26@users.noreply.github.com>
2026-04-19 10:49:46 +0700

    Update AudioToWater.cs

--- Assets/Scripts/Interaction/AudioToWater.cs
+++ Assets/Scripts/Interaction/AudioToWater.cs
@@ -1,1 +1,17 @@
+using UnityEngine;
 
+public class AudioToWater : MonoBehaviour
+{
+    public WaterController water;
+
+    void Update()
+    {
+        float value = AudioAnalyzer.audioValue;
+
+        if (value > 0.01f)
+        {
+            int index = Random.Range(0, water.pointCount);
+            water.Splash(index, value);
+        }
+    }
+}
