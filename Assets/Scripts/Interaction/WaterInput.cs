8063b356afd0c6fb95b373c9a169871ec9465144
wzr26 <127534016+wzr26@users.noreply.github.com>
2026-04-19 10:48:58 +0700

    Update WaterInput.cs

--- Assets/Scripts/Interaction/WaterInput.cs
+++ Assets/Scripts/Interaction/WaterInput.cs
@@ -1,1 +1,15 @@
+using UnityEngine;
 
+public class WaterInput : MonoBehaviour
+{
+    public WaterController water;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            int index = Random.Range(0, water.pointCount);
+            water.Splash(index, 0.5f);
+        }
+    }
+}
