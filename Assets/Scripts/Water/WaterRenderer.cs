33c2d9a9415951b68c099c4e2e10608df3584d7a
wzr26 <127534016+wzr26@users.noreply.github.com>
2026-04-19 10:47:25 +0700

    Update WaterRenderer.cs

--- Assets/Scripts/Water/WaterRenderer.cs
+++ Assets/Scripts/Water/WaterRenderer.cs
@@ -1,1 +1,25 @@
+using UnityEngine;
 
+[RequireComponent(typeof(LineRenderer))]
+public class WaterRenderer : MonoBehaviour
+{
+    public WaterController water;
+    LineRenderer line;
+
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        line.positionCount = water.pointCount;
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < water.pointCount; i++)
+        {
+            float x = i * water.width / water.pointCount;
+            float y = water.points[i].height;
+
+            line.SetPosition(i, new Vector3(x, y, 0));
+        }
+    }
+}
